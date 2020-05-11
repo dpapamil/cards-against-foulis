9a62577c445353ad9f965f7a22d7adbc59f3af10
Dimitrios Papamilios <dimitrios.papamilios@autodesk.com>
2020-05-11 17:13:34 +0100

    Add cases for the rest of message types

--- app/src/test/java/com/papamilios/dimitris/cardsagainstfoulis/MessageTest.java
+++ app/src/test/java/com/papamilios/dimitris/cardsagainstfoulis/MessageTest.java
@@ -5,12 +5,14 @@ package com.papamilios.dimitris.cardsagainstfoulis;
  */
 
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.AskCardMessage;
+import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.ChatMessage;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.ChooseWhiteCardMessage;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.ChooseWinnerMessage;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.EndRoundMessage;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.IMessageVisitor;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.SendCardMessage;
 import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.StartRoundMessage;
+import com.papamilios.dimitris.cardsagainstfoulis.controller.messages.SwapCardsMessage;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -97,6 +99,28 @@ public class MessageTest {
         String cardText = "Mpoumpounea";
         SendCardMessage msg = SendCardMessage.create(cardText);
         assertEquals(msg.cardText(), cardText);
+
+        // Test Visitor
+        msg.accept(messageVisitor);
+        verify(messageVisitor, times(1)).visit(msg);
+    };
+
+    @Test
+    public void testChatMessage() {
+        // Test the message
+        String chat = "Mpoumpounea";
+        ChatMessage msg = ChatMessage.create(chat);
+        assertEquals(msg.getText(), chat);
+
+        // Test Visitor
+        msg.accept(messageVisitor);
+        verify(messageVisitor, times(1)).visit(msg);
+    };
+
+    @Test
+    public void testSwapCardsMessage() {
+        // Test the message
+        SwapCardsMessage msg = SwapCardsMessage.create();
 
         // Test Visitor
         msg.accept(messageVisitor);
